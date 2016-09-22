a38962b1e032bdc22cd8c4407ddeabe51dcc7622
lcy <lichunyang@stu.xidian.edu.cn>
2016-09-22 21:24:48 +0800

    find a big big big bug

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'', views.index,name='index'),
+    url(r'getstatus', views.getstatus,name='getstatus'),    
+    url(r'changeport', views.changeport,name='changeport'),      
 	]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-
+import json
 from gpio import port
 # Create your views here.
 
@@ -13,13 +13,17 @@ def index(request):
     content={
         'ports':[7,11,12,13,15,16,18,22,29,30,31,32,33,35,36,37,38,40],
         'maxport':40,
-        'highport':[],
     }
+    return render(request,"index.html",content)
+
+
+@csrf_exempt
+def getstatus(request):
+    return HttpResponse(json.dumps(port.getstatus()))
+
+@csrf_exempt
+def changeport(request):
     if request.method=="POST":
         if request.POST.get("port",""):
             result = port.changestat(int(request.POST.get("port","")))
-            content['highport']=port.getstatus()
-            return HttpResponse(result)
-    else:
-        content['highport'] = port.getstatus()
-        return render(request,"index.html",content)
+            return HttpResponse(result)    
