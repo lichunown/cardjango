7ce34648d4dfd7c0cac5fc111fe2e84910c73df0
lcy <lichunyang@stu.xidian.edu.cn>
2016-10-09 13:22:41 +0800

    debug views

--- gpio/port.py
+++ gpio/port.py
@@ -4,7 +4,10 @@ import time
 ports=[7,11,12,13,15,16,18,22,29,31,32,33,35,36,37,38,40]
 
 def init():
-    GPIO.setmode(GPIO.BOARD)
+    try:
+        GPIO.setmode(GPIO.BOARD)
+    except Exception,e:
+        print e
     for item in ports:
         try:
             GPIO.setup(item,GPIO.OUT)
--- main/views.py
+++ main/views.py
@@ -5,20 +5,21 @@ import json
 from gpio import port,run
 # Create your views here.
 
-try:
-    port.init()
-except Exception,e:
-    print e
+# try:
+#     port.init()
+# except Exception,e:
+#     print e
 
 car = run.Car()
 
 @csrf_exempt
 def index(request):
-    content={
-        'ports':[7,11,12,13,15,16,18,22,29,30,31,32,33,35,36,37,38,40],
-        'maxport':40,
-    }
-    return render(request,"index.html",content)
+    # content={
+    #     'ports':[7,11,12,13,15,16,18,22,29,30,31,32,33,35,36,37,38,40],
+    #     'maxport':40,
+    # }
+    # return render(request,"index.html",content)
+    pass
 
 
 @csrf_exempt
